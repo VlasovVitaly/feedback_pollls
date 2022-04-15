1741526669ba673feb755b0269a899f9e6aba954
Власов Виталий <vitaly.vlasov@automash-mb.ru>
2022-04-15 15:43:48 +0300

    Small style fixups

--- feedback/models.py
+++ feedback/models.py
@@ -10,6 +10,17 @@ from tortoise.models import Model, Q
 from feedback.utils import generate_random_string
 
 
+ALL_STAT_PERIODS = {
+    'day': 'сегодня',
+    'week': 'неделю',
+    'month': 'месяц',
+    'year': 'год'
+}
+
+
+DEFAULT_STAT_PERIOD = 'day'
+
+
 class Poll(Model):
     url_code = fields.CharField(max_length=10, unique=True, null=True, index=True)
     vote = fields.SmallIntField(null=True, index=True)
@@ -31,26 +42,6 @@ class Poll(Model):
         return f'[{self.id}] {self.url_code} -> {self.vote}'
 
 
-ALL_STAT_PERIODS = {
-    'day': 'сегодня',
-    'week': 'неделю',
-    'month': 'месяц',
-    'year': 'год'
-}
-
-DEFAULT_STAT_PERIOD = 'day'
-
-
-rating_annotations = {
-    'total': Count('id'),
-    'rate1': Count('id', _filter=Q(vote=1)),
-    'rate2': Count('id', _filter=Q(vote=2)),
-    'rate3': Count('id', _filter=Q(vote=3)),
-    'rate4': Count('id', _filter=Q(vote=4)),
-    'rate5': Count('id', _filter=Q(vote=5)),
-}
-
-
 def calc_rating(ratings):
     rating = {
         'rating': 0, 'total': ratings.total,
@@ -65,8 +56,7 @@ def calc_rating(ratings):
         return rating
 
     all_ratings = zip(
-        itemgetter('rate1', 'rate2', 'rate3', 'rate4', 'rate5')(rating),
-        (1, 2, 3, 4, 5)
+        itemgetter('rate1', 'rate2', 'rate3', 'rate4', 'rate5')(rating), (1, 2, 3, 4, 5)
     )
 
     total_points = reduce(lambda prev, val: prev + (val[0] * val[1]), all_ratings, 0)
@@ -76,6 +66,15 @@ def calc_rating(ratings):
 
 
 async def get_stats(period):
+    rating_annotations = {
+        'total': Count('id'),
+        'rate1': Count('id', _filter=Q(vote=1)),
+        'rate2': Count('id', _filter=Q(vote=2)),
+        'rate3': Count('id', _filter=Q(vote=3)),
+        'rate4': Count('id', _filter=Q(vote=4)),
+        'rate5': Count('id', _filter=Q(vote=5)),
+    }
+
     query_filters = {
         'day': {'voted_date': date.today()},
         'week': {'voted_date__gt': date.today() - timedelta(weeks=1)},
